f17e50b3dc2c2fa52059636bfd0ae704c5f2b537
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2024-03-18 12:00:17 +0300

    Constructors for log

--- log/src/main/java/ru/draen/tpo/log/Log5.java
+++ log/src/main/java/ru/draen/tpo/log/Log5.java
@@ -1,8 +1,16 @@
 package ru.draen.tpo.log;
 
 public class Log5 extends AbstractLogFunction {
-    private final Ln ln = new Ln();
+    private final Ln ln;
     private static final double LN5 = 1.60943791243;
+
+    public Log5() {
+        this.ln = new Ln();
+    }
+
+    public Log5(Ln ln) {
+        this.ln = ln;
+    }
 
     @Override
     public double calculate(double x, double eps) {
